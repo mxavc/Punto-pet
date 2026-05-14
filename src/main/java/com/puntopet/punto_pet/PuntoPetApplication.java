12c129969404f44ad13108224a9dcb982be0cfe1
Marlon Xavier Chimarro Bermeo <max_inm@outlook.com>
2026-05-13 19:57:28 -0500

    feat: configuracion gitignore y raiz del proyecto con dependencias

---
+++ src/main/java/com/puntopet/punto_pet/PuntoPetApplication.java
@@ -0,0 +1,13 @@
+package com.puntopet.punto_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntoPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntoPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puntopet/punto_pet/PuntoPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puntopet.punto_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntoPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
